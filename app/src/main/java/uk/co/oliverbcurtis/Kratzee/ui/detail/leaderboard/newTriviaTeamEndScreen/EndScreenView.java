11844d86208ae17c6344f0361a0bb73fdc9eadc9
Olly <olly_915@hotmail.com>
2018-11-27 23:56:20 +0000

    Bug Fixes

--- app/src/main/java/uk/co/oliverbcurtis/Kratzee/ui/detail/leaderboard/newTriviaTeamEndScreen/EndScreenView.java
+++ app/src/main/java/uk/co/oliverbcurtis/Kratzee/ui/detail/leaderboard/newTriviaTeamEndScreen/EndScreenView.java
@@ -14,45 +14,58 @@ import uk.co.oliverbcurtis.Kratzee.ui.detail.startScreen.StartScreenView;
 
 public class EndScreenView extends BaseActivity implements View.OnClickListener  {
 
-    private TextView tv_points_display;
-    private ImageView btn_home;
+  private TextView tv_points_display;
+  private ImageView btn_home;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.points_summary);
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.points_summary);
 
-        initView();
+    initView();
+  }
+
+  public void initView(){
+
+    tv_points_display = findViewById(R.id.tv_points_display);
+    tv_points_display.setText("All Team-Members Will Receive "+Score.getScore()+" Points For This Session, You May Now Close The App!");
+
+    btn_home = findViewById(R.id.btn_home);
+    btn_home.setOnClickListener(this);
+
+  }
+
+  @Override
+  public void onClick(View view) {
+
+    switch (view.getId()) {
+
+      case R.id.btn_home:
+        backToMainMenu();
+        break;
+
     }
-
-    public void initView(){
-
-        tv_points_display = findViewById(R.id.tv_points_display);
-        tv_points_display.setText("All Team-Members Will Receive "+Score.getScore()+" Points For This Session, You May Now Close The App!");
-
-        btn_home = findViewById(R.id.btn_home);
-        btn_home.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        switch (view.getId()) {
-
-            case R.id.btn_home:
-                backToMainMenu();
-                break;
-
-        }
-    }
+  }
 
 
-    public void backToMainMenu(){
+  public void backToMainMenu(){
 
-        Score.resetScore();
+    Score.resetScore();
 
-        Intent intent = new Intent(getApplicationContext(), StartScreenView.class);
-        startActivity(intent);
-    }
+    showToast(this, "You have Now Completed both the Individual Quiz & Team Quiz! Well Done!");
+
+    Intent intent = new Intent(getApplicationContext(), StartScreenView.class);
+    startActivity(intent);
+  }
+
+  @Override
+  public void onBackPressed() {
+
+    Score.resetScore();
+
+    showToast(this, "You have Now Completed both the Individual Quiz & Team Quiz! Well Done!");
+
+    Intent intent = new Intent(getApplicationContext(), StartScreenView.class);
+    startActivity(intent);
+  }
 }
